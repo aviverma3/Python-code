f0bbd3171abc66ca6dc6b5383ad25793287635dd
Avinash <avinash.verma.java@gmail.com>
2024-08-14 22:51:35 +0530

    changes done for inheritance

--- test3.py
+++ test3.py
@@ -5,8 +5,32 @@ class Student:
         self.name = name
         self.marks = marks
         self.rollnum = rollnum
+    @staticmethod #decorator
+    def hello():
+        print("hello")
+
+    def dispay(self):
+        print(self.name, self.marks, self.rollnum)
 
 s1 = Student("avinash", 90, 234459)
 print(s1.name, s1.rollnum)
+
+s1.hello()
+
 s2 = Student("pallavi", 91, 83029)
-print(s2.name, s2.marks)
+s2.dispay()
+
+class chemistry(Student):
+    name = "breaking bad"
+    @staticmethod
+    def subject():
+        print("this is chemistry class")
+    
+ch1 = chemistry("ravi", 99, 42324)
+ch1.dispay()
+ch1.subject()
+s3 = Student("naman", 97, 324324)
+s3.dispay()
+print(ch1.name)
+
+# ch1.subject()
